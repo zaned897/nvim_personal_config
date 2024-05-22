e2c0c4f330370a31d62c0f58197e17520cb2efd4
zaned897 <3dmena@gmail.com>
2024-05-22 11:08:16 -0600

    bufferline and nvim tree configured

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,4 +8,7 @@ return {
       separator_style = "thin",
     },
   },
+			mode = "buffers",
+			separator_style = "thin", -- You can change to "slant" or "padded_slant" for different styles
+			separator_icon = "│", -- Modern and simple vertical bar
 }

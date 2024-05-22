e1634e12f1e285a19d0a7d3ef5d1b039cad8f204
zaned897 <3dmena@gmail.com>
2024-05-22 11:09:43 -0600

    feat: conf updated bufferline/nvim-tree

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,14 +1,21 @@
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  version = "*",
-  opts = {
-    options = {
-      mode = "tabs",
-      separator_style = "thin",
-    },
-  },
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
 			mode = "buffers",
 			separator_style = "thin", -- You can change to "slant" or "padded_slant" for different styles
 			separator_icon = "│", -- Modern and simple vertical bar
+			diagnostics = "nvim_lsp",
+			always_show_bufferline = true,
+			show_buffer_close_icons = true,
+			show_close_icon = false,
+			show_tab_indicators = true,
+			indicator = {
+				style = "icon",
+				icon = "▎",
+			},
+		},
+	},
 }

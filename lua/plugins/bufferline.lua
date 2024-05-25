f1102ee8e6f2dbd9835e7410925e0208351373c5
zaned897 <3dmena@gmail.com>
2024-05-24 18:11:54 -0600

    feat: bufferline navigation on leader b2

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -2,20 +2,27 @@ return {
 	"akinsho/bufferline.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	version = "*",
-	opts = {
-		options = {
-			mode = "buffers",
-			separator_style = "thin", -- You can change to "slant" or "padded_slant" for different styles
-			separator_icon = "│", -- Modern and simple vertical bar
-			diagnostics = "nvim_lsp",
-			always_show_bufferline = true,
-			show_buffer_close_icons = true,
-			show_close_icon = false,
-			show_tab_indicators = true,
-			indicator = {
-				style = "icon",
-				icon = "▎",
+	config = function()
+		require("bufferline").setup({
+			options = {
+				mode = "buffers",
+				separator_style = "thin", -- Puedes cambiar a "slant" o "padded_slant" para diferentes estilos
+				separator_icon = "│", -- Barra vertical moderna y simple
+				diagnostics = "nvim_lsp",
+				always_show_bufferline = true,
+				show_buffer_close_icons = true,
+				show_close_icon = false,
+				show_tab_indicators = true,
+				indicator = {
+					style = "icon",
+					icon = "▎",
+				},
 			},
-		},
-	},
+		})
+		-- Mapeos para moverte entre buffers
+		vim.api.nvim_set_keymap("n", "<Leader>bn", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap("n", "<Leader>bp", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+		-- Mapeos para cerrar buffers
+		vim.api.nvim_set_keymap("n", "<Leader>bd", ":bdelete<CR>", { noremap = true, silent = true })
+	end,
 }
